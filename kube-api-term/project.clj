95054d7cc97e46219f579003159f727509299ce3
Paul Rutledge <rutledgepaulv@gmail.com>
2020-11-21 23:21:28 -0600

    add terminal emulator that connects to pod

---
+++ kube-api-term/project.clj
@@ -0,0 +1,39 @@
+(defproject org.clojars.rutledgepaulv/kube-api-term "0.1.0-SNAPSHOT"
+
+  :description
+  "A Kubernetes controller library for Clojure."
+
+  :url
+  "https://github.com/rutledgepaulv/kube-api"
+
+  :license
+  {:name "MIT License" :url "http://opensource.org/licenses/MIT" :year 2020 :key "mit"}
+
+  :scm
+  {:dir ".."}
+
+  :pom-addition
+  [:developers
+   [:developer
+    [:name "Paul Rutledge"]
+    [:url "https://github.com/rutledgepaulv"]
+    [:email "rutledgepaulv@gmail.com"]
+    [:timezone "-5"]]]
+
+  :repositories
+  [["jcenter" {:url "https://jcenter.bintray.com"}]]
+
+  :deploy-repositories
+  [["releases" :clojars]
+   ["snapshots" :clojars]]
+
+  :aot
+  [kube-api.term.term]
+
+  :dependencies
+  [[org.clojure/clojure "1.10.1"]
+   [org.clojars.rutledgepaulv/kube-api-core "0.1.0-SNAPSHOT"]
+   [org.jetbrains.jediterm/jediterm-pty "2.31"]
+   [com.google.guava/guava "30.0-jre"]
+   [com.rpl/proxy-plus "0.0.5"]
+   [log4j/log4j "1.2.17"]])
---
+++ kube-api-term/test/kube_api/term/core_test.clj
@@ -0,0 +1,7 @@
+(ns kube-api.term.core-test
+  (:require [clojure.test :refer :all]
+            [kube-api-term.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
