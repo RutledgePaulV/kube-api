9f180584962ea8423e512cab22b33368effad652
Paul Rutledge <rutledgepaulv@gmail.com>
2020-11-29 00:46:41 -0600

    adding support for integration tests with kind

--- kube-api-test/project.clj
+++ kube-api-test/project.clj
@@ -1,5 +1,29 @@
 (defproject kube-api/kube-api-test "0.1.0-SNAPSHOT"
 
+  :description
+  "A library for testing kubernetes libraries and applications from Clojure."
+
+  :url
+  "https://github.com/rutledgepaulv/kube-api/kube-api-test"
+
+  :license
+  {:name "MIT License" :url "http://opensource.org/licenses/MIT" :year 2020 :key "mit"}
+
+  :scm
+  {:name "git" :url "https://github.com/rutledgepaulv/kube-api"}
+
+  :pom-addition
+  [:developers
+   [:developer
+    [:name "Paul Rutledge"]
+    [:url "https://github.com/rutledgepaulv"]
+    [:email "rutledgepaulv@gmail.com"]
+    [:timezone "-5"]]]
+
+  :deploy-repositories
+  [["releases" :clojars]
+   ["snapshots" :clojars]]
+
   :dependencies
   [[org.clojure/clojure "1.10.1"]
    [org.testcontainers/testcontainers "1.15.0"]
