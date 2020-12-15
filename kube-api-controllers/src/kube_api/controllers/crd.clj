a921a0446975b820b487a9f9e3008e3669db08b5
Paul Rutledge <rutledgepaulv@gmail.com>
2020-12-15 07:38:49 -0600

    - add support for crd

--- kube-api-controllers/src/kube_api/controllers/crd.clj
+++ kube-api-controllers/src/kube_api/controllers/crd.clj
@@ -1,79 +1,112 @@
 (ns kube-api.controllers.crd
   (:require [kube-api.core.core :as kube]
             [clojure.string :as strings]
-            [malli.json-schema :as mjs]))
+            [malli.json-schema :as mjs]
+            [kube-api.core.utils :as utils]))
+
+(defn malli->openapi3 [malli]
+  (mjs/transform malli))
+
+
+(defn create-crd-definition [group name schema {:keys [plural version scope short-names] :as options}]
+  (letfn [(get-plural []
+            (strings/lower-case (or plural (str name "s"))))
+          (get-singular []
+            (strings/lower-case name))
+          (get-fully-qualified-name []
+            (str (get-plural) "." group))
+          (get-version []
+            (or version "v1"))
+          (get-scope []
+            (or scope "Namespaced"))
+          (get-short-names []
+            (or short-names []))
+          (get-group []
+            (strings/lower-case group))
+          (get-kind []
+            name)]
+    {:apiVersion "apiextensions.k8s.io/v1"
+     :kind       "CustomResourceDefinition"
+     :metadata   {:name (get-fully-qualified-name)}
+     :spec       {:group    (get-group)
+                  :scope    (get-scope)
+                  :versions [{:name    (get-version)
+                              :served  true
+                              :storage true
+                              :schema  {:openAPIV3Schema (malli->openapi3 schema)}}]
+                  :names    {:plural     (get-plural)
+                             :singular   (get-singular)
+                             :kind       (get-kind)
+                             :shortNames (get-short-names)}}}))
+
+
+(defn update-crd-definition [existing group name schema {:keys [plural version scope short-names] :as options}]
+  (letfn [(get-plural []
+            (strings/lower-case (or plural (str name "s"))))
+          (get-group []
+            (strings/lower-case group))
+          (get-kind []
+            name)
+          (get-singular []
+            (strings/lower-case name))
+          (get-fully-qualified-name []
+            (str (get-plural) "." (get-group)))
+          (get-version []
+            (or version "v1"))
+          (get-short-names []
+            (or short-names []))]
+    (let [versions     (utils/index-by :name (get-in existing [:spec :versions]))
+          new-version  {:name (get-version) :served true :schema {:openAPIV3Schema (malli->openapi3 schema)}}
+          new-versions (update versions (get-version) (fnil merge {}) new-version)]
+      {:apiVersion "apiextensions.k8s.io/v1"
+       :kind       "CustomResourceDefinition"
+       :metadata   {:name            (get-fully-qualified-name)
+                    :resourceVersion (get-in existing [:metadata :resourceVersion])}
+       :spec       {:group    (get-group)
+                    :scope    (get-in existing [:spec :scope])
+                    :versions (vec (vals new-versions))
+                    :names    {:plural     (get-plural)
+                               :singular   (get-singular)
+                               :kind       (get-kind)
+                               :shortNames (get-short-names)}}})))
 
 
 (defn ensure-installed
+  "Given a kubernetes client, an api group name, a crd name, and a malli schema describing
+   the resource (only need to specify one top level key `spec` because metadata is specified
+   by kubernetes itself), then install and/or update the resource definition in the cluster
+   and return the response.
+
+   (def friend-schema [:map [:spec [:favoriteColor [:enum \"Blue\" \"Green\"]]]])
+   (ensure-installed client \"shippy.io\" \"Friend\" friend-schema)
+
+   Intended for use by kubernetes controllers / operators that install their own schemas.
+   "
   ([client group name schema]
    (ensure-installed client group name schema {}))
   ([client group name schema {:keys [plural version scope short-names] :as options}]
-   (letfn [(get-plural []
-             (strings/lower-case (or plural (str name "s"))))
-           (get-singular []
-             (strings/lower-case name))
-           (get-fully-qualified-name []
-             (str (get-plural) "." group))
-           (get-version []
-             (or version "v1"))
-           (get-scope []
-             (or scope "Namespaced"))
-           (get-short-names []
-             (or short-names []))
-           (exists? [response]
+   (letfn [(exists? [response]
              (not= 404 (:code response)))
            (conflicts? [response]
              (= 409 (:code response)))
-           (malli->openapi3 [malli-schema]
-             (mjs/transform malli-schema))
-           (index-by [f coll]
-             (into {} (map (juxt f identity)) coll))
-           (create-payload []
-             {:apiVersion "apiextensions.k8s.io/v1"
-              :kind       "CustomResourceDefinition"
-              :metadata   {:name (get-fully-qualified-name)}
-              :spec       {:group    group
-                           :scope    (get-scope)
-                           :versions [{:name    (get-version)
-                                       :served  true
-                                       :storage true
-                                       :schema  {:openAPIV3Schema (malli->openapi3 schema)}}]
-                           :names    {:plural     (get-plural)
-                                      :singular   (get-singular)
-                                      :kind       name
-                                      :shortNames (get-short-names)}}})
-           (update-payload [existing]
-             (let [versions     (index-by :name (get-in existing [:spec :versions]))
-                   new-version  {:name (get-version) :served true :schema {:openAPIV3Schema (malli->openapi3 schema)}}
-                   new-versions (update versions (get-version) (fnil merge {}) new-version)]
-               {:apiVersion "apiextensions.k8s.io/v1"
-                :kind       "CustomResourceDefinition"
-                :metadata   {:name            (get-fully-qualified-name)
-                             :resourceVersion (get-in existing [:metadata :resourceVersion])}
-                :spec       {:group    group
-                             :scope    (get-in existing [:spec :scope])
-                             :versions (vec (vals new-versions))
-                             :names    {:plural     (get-plural)
-                                        :singular   (get-singular)
-                                        :kind       name
-                                        :shortNames (get-short-names)}}}))
-           (get-resource []
+           (get-resource [name]
              (let [op-selector {:kind "CustomResourceDefinition" :action "get"}
-                   request     {:path-params {:name (get-fully-qualified-name)}}]
+                   request     {:path-params {:name name}}]
                (kube/invoke client op-selector request)))
-           (create-resource []
+           (create-resource [create-payload]
              (let [op-selector {:kind "CustomResourceDefinition" :action "create"}
-                   request     {:body (create-payload)}]
+                   request     {:body create-payload}]
                (kube/invoke client op-selector request)))
-           (update-resource [existing]
+           (update-resource [update-payload]
              (let [op-selector {:kind "CustomResourceDefinition" :action "update"}
-                   request     {:body        (update-payload existing)
-                                :path-params {:name (get-in existing [:metadata :name])}}]
+                   request     {:body        update-payload
+                                :path-params {:name (get-in update-payload [:metadata :name])}}]
                (kube/invoke client op-selector request)))]
-     (let [existing (get-resource)
-           response (if (exists? existing)
-                      (update-resource existing)
-                      (create-resource))]
+     (let [create-payload (create-crd-definition group name schema options)
+           existing       (get-resource (get-in create-payload [:metadata :name]))
+           response       (if (exists? existing)
+                            (update-resource (update-crd-definition existing group name schema options))
+                            (create-resource create-payload))]
        (if (conflicts? response)
-         (ensure-installed client group name schema options)
+         (recur client group name schema options)
          response)))))
