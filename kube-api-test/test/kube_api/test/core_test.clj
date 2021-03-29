8f037923c5bca073b0782ef0f1f0ea8f11845d54
Paul Rutledge <rutledgepaulv@gmail.com>
2021-03-29 07:52:40 -0500

    add missing file

---
+++ kube-api-test/test/kube_api/test/core_test.clj
@@ -0,0 +1,7 @@
+(ns kube-api.test.core-test
+  (:require [clojure.test :refer :all]
+            [kube-api.test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
